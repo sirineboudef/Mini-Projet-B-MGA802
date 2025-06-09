5f24727de32367ddcaed13b8733f6e18c973bca4
syrine <syrine.boudef.1@ens.etsmtl.ca>
2025-06-09 17:10:07 -0400

    Importation de modules necessaires en simplifiant le code

--- Methode_rectangle_Numpay.py
+++ Methode_rectangle_Numpay.py
@@ -1,29 +1,7 @@
 import timeit
 import numpy as np
-
-def f(x: float, p1: float, p2: float, p3: float, p4: float):
-    """Fonction polynomiale du troisième degré."""
-    return p1 + p2 * x + p3 * x ** 2 + p4 * x ** 3
-
-
-def integral_analytique(a: float, b: float, p1: float, p2: float, p3: float, p4: float):
-    """Intégrale analytique de f entre a et b"""
-    term1 = p1 * (b - a)
-    term2 = p2 * (b ** 2 - a ** 2) / 2.0
-    term3 = p3 * (b ** 3 - a ** 3) / 3.0
-    term4 = p4 * (b ** 4 - a ** 4) / 4.0
-    return term1 + term2 + term3 + term4
-
-
-def integral_rectangle(a: float, b: float, p1: float, p2: float, p3: float, p4: float, n: int = 10):
-    """Intégration numérique par la méthode des rectangles sur n segments (version classique)."""
-    h = (b - a) / n  # largeur d'un segment
-    total = 0
-    x = a
-    for _ in range(n):
-        total += f(x, p1, p2, p3, p4) * h
-        x += h
-    return total
+from polynome import f
+from methode_analytique import integrale_analytique
 
 
 def integral_rectangle_numpy(a: float, b: float, p1: float, p2: float, p3: float, p4: float, n: int = 10):
