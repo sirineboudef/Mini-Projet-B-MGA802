2679f07b8bb3df89b294287e1d0671370fc7699d
paul1 <paulin.muhlhoff@gmail.com>
2025-06-10 21:17:27 -0400

    Correction bug simpson scipy

--- methode_simpson_scipy.py
+++ methode_simpson_scipy.py
@@ -4,16 +4,16 @@ import numpy as np
 
 def integrale_simpson_scipy(a: float, b: float, p1: float, p2: float, p3: float, p4: float, n: int = 10):
     """Intégration numérique par la méthode de Simpson sur n segments. Version pré-programmée SciPy"""
-    x = np.linspace(a, b, num=n)
+    x = np.linspace(a, b, n)
     y = f(x, p1, p2, p3, p4)
-    return simpson(y, x=None, dx=1.0, axis=-1)
+    return simpson(y,x=x)
 
 # -------------- Test du module --------------
 
 # Valeurs fixes pour les coefficients du polynome et limites d'intégration
-p1, p2, p3, p4 = 26, 36, 12, 7  # coefficients du polynôme
-a, b = -50, 50  # bornes de l'intégrale
-n = 11  # nombre de points pour les méthodes
+# p1, p2, p3, p4 = 26, 36, 12, 7  # coefficients du polynôme
+# a, b = -50, 50  # bornes de l'intégrale
+# n = 10  # nombre de points pour les méthodes
 
 # Test de la fonction
-# print(integrale_simpson_scipy(a, b, p1, p2, p3, p4, n))
+print(integrale_simpson_scipy(-50, 50, 26, 36, 12, 7, 1000))
